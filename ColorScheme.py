e2ede1eb63db72fa7f7511c77e1c69640888364a
Michael Duo Ling <duo.ling.cn@gmail.com>
2015-01-19 11:41:38 +0100

    ...

---
+++ ColorScheme.py
@@ -0,0 +1,22 @@
+from termcolor import colored
+
+active_c = "cyan"
+active_legendary_c = "magenta"
+deactive_c = "white"
+warn_c = "red"
+improvable_c = "blue"
+
+def active_str(s):
+    return colored(s, active_c)
+
+def active_legendary_str(s):
+    return colored(s, active_legendary_c)
+
+def deactive_str(s):
+    return colored(s, deactive_c)
+
+def warn_ste(s):
+    return colored(s, warn_c)
+
+def improvable_str(s):
+    return colored(s, improvable_c)
--- Gems.py
+++ Gems.py
@@ -1,4 +1,5 @@
 from Unit import Unit
+from ColorScheme import *
 from termcolor import colored
 
 class Gem(Unit):
@@ -14,18 +15,31 @@ class LegendaryGem(Gem):
     def apply(self):
         pass
     def __str__(self):
-        return super().__str__() + """
-Level:  {lvl}""".format(lvl=self.lvl)
+        return super().__str__() + colored("""
+Level:  {lvl}""".format(lvl=self.lvl), active_legendary_c)
 
 class GemOfEfficaciousToxin(LegendaryGem):
     def __init__(self, lvl):
         super().__init__("Gem of Efficacious Toxin", lvl)
+        self.poison = 2000 + 50 * self.lvl
     def __str__(self):
-        return super().__str__() + """
+        result = super().__str__()
+        result += active_legendary_str("""
 Properties:
-    * Poison all enemies hit for """ + colored("2000%", "blue") + """ damage as Poison over 10 seconds.
-        * Upgrade: +50% damage as Poison over 10 seconds (+5% per second) per Rank.
-    * Poisoned enemies also take 10% increased damage from all sources (Requires Rank 25)"""
+    * Poison all enemies hit for """)
+        result += improvable_str("{}%".format(self.poison))
+        result += active_legendary_str(""" damage as Poison over 10 seconds.
+        * Upgrade: +50% damage as Poison over 10 seconds (+5% per second) per Rank.""")
+        if self.lvl >= 25:
+            tmp_str = active_legendary_str
+            tmp_lvl_str = active_legendary_str
+        else:
+            tmp_str = deactive_str
+            tmp_lvl_str = warn_str
+        result += tmp_str("""
+    * Poisoned enemies also take 10% increased damage from all sources (""")
+        result += tmp_lvl_str("Requires Rank 25") + tmp_str(")")
+        return result
 
 if __name__ == '__main__':
     from colorama import Fore, Back, Style
